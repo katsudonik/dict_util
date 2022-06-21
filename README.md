8d1e46527f589e42c44ceaa2f7a3d101bb7be587
kaikeda <kaikeda@ikedakatsuyukinoiMac.local>
2022-06-21 18:24:47 +0900

    update description

--- README.md
+++ README.md
@@ -7,6 +7,14 @@ python dict utility
 pip install dict_util
 ```
 
+
+## functions
+
+- convert dict to dataframe
+- convert dataframe to dict
+- compare dict on dataframe
+- compare dict on dataframe & merge differences
+
 ## usage
 
 See: [usage.ipynb](https://github.com/katsudonik/dict_util/blob/main/usage.ipynb)
--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ import setuptools
  
 setuptools.setup(
     name="dict_util",
-    version="1.4.1",
+    version="1.4.2",
     license='MIT',
     install_requires=['numpy', 'pandas'],
     author="katsudonik",
     author_email="kaikeda.work@gmail.com",
     description="python dict utility package",
-    long_description="python dict utility package",
+    long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     keywords='dict, df, dict to df, df to dict, compare dict',
     url="https://github.com/katsudonik/dict_util",
